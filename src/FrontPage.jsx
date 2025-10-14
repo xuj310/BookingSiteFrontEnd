9d12361acdad5128daaaa982f4a5b1c08c9a1eae
xuj310 <xudongj95@gmail.com>
2025-10-14 16:04:56 +1100

    clean up front page
    clean up front page

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -1,12 +1,5 @@
 import { Fragment } from "react";
 import Container from "react-bootstrap/Container";
-import { Row, Col, Image } from "react-bootstrap";
-
-const items = [
-  { id: 1, title: "Board Games @ Clayton", img: "https://sxcu.net/7kw4ILQyy.png" },
-  { id: 2, title: "Tennis Meet @ Doncaster", img: "https://sxcu.net/7kw4ILQyy.png" },
-  { id: 3, title: "Oil Painting Class @ Ringwood", img: "https://sxcu.net/7kw4ILQyy.png" },
-];
 
 const FrontPage = () => {
   return (
@@ -14,18 +7,11 @@ const FrontPage = () => {
       <Container>
         <div className="cardAlignment">
           <div className="welcome-box">
-            <h3>Welcome to BookingsSite!</h3>
+            <h3>Welcome to Bookings Site!</h3>
             <div className="listContainer">
-              {items.map((item) => (
-                <Row key={item.id} className="listRow">
-                  <Col xs={3}>
-                    <Image src={item.img} fluid rounded />
-                  </Col>
-                  <Col xs={9} className="titleCol">
-                    <h5>{item.title}</h5>
-                  </Col>
-                </Row>
-              ))}
+              <h4>Register an account to get started. <br></br>
+              Otherwise you can Only view the events without an account.</h4>
+
             </div>
           </div>
         </div>
