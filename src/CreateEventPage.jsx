5a9f9b8230298d3417fc458a4e5d745c9d428035
xuj310 <xudongj95@gmail.com>
2025-10-15 14:53:04 +1100

    Added comments to CreateEventPage

--- src/CreateEventPage.jsx
+++ src/CreateEventPage.jsx
@@ -1,3 +1,16 @@
+/*
+  useState - Tracks state so you can track/update values
+  Fragment - Let's you group multiple elements without extra DOM nodes
+  useRef - Enables access to specific DOM elements that persists across renders
+  useEffect - Runs effects after render, such as data fetching
+
+  Container - Bootstrap layout component
+  toast - a function for triggering pop up 'toast' notifications
+  Floater - a tooltip component that floats near the target element
+  useNavigate - programmatically change routes
+  jwtDecode - used to decode JWTs  
+*/
+
 import { useState, Fragment, useRef, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import { toast } from "react-toastify";
@@ -5,12 +18,18 @@ import Floater from "react-floater";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
+/*
+  Page for creating a new event. Can only be accessed when logged in and returns the user to the events page when complete.
+*/
 const CreateEventPage = () => {
+  // Event field variables
   const [imgUrl, setImgUrl] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState(null);
+  // Collect all the errors here
   const [errors, setErrors] = useState([]);
+  // We need this to place the floater in the right place
   const formRef = useRef(null);
 
   const navigate = useNavigate();
@@ -19,6 +38,7 @@ const CreateEventPage = () => {
   useEffect(() => {
     const token = sessionStorage.getItem("token");
 
+    // Return the user to the front page if they navigate here without an auth token
     if (!token) {
       toast("You must be logged in to create an event");
       navigate("/");
@@ -26,10 +46,12 @@ const CreateEventPage = () => {
     }
 
     try {
+      // Decode the token
       const payload = jwtDecode(token);
        // Current time in seconds
       const now = Math.floor(Date.now() / 1000);
 
+      // If it's invalid then return to the front page
       if (!payload.exp || payload.exp < now) {
         toast("Session expired. Please log in again.");
         navigate("/");
@@ -49,7 +71,7 @@ const CreateEventPage = () => {
 
   let errorFloater = null;
 
-  // Show a floater with the errors
+  // Show a floater with the errors. We also need to format it as a list of errors.
   if (errors.length > 0) {
     errorFloater = (
       <Floater
@@ -74,12 +96,7 @@ const CreateEventPage = () => {
     e.preventDefault();
     let parsed;
 
-    const token = sessionStorage.getItem("token");
-    if (!token) {
-      toast("Not logged in");
-    }
-
-    // Create an event
+    // Create an event by calling the backend
     try {
       const res = await fetch("http://localhost:5000/api/events", {
         method: "POST",
@@ -90,6 +107,7 @@ const CreateEventPage = () => {
         body: JSON.stringify({ imgUrl, title, description, date }),
       });
 
+      // Obtain the response 
       const data = await res.text();
       const parsed = JSON.parse(data);
 
@@ -106,6 +124,9 @@ const CreateEventPage = () => {
     }
   };
 
+  /*
+    The layout of the page. It's a standard form and it will call handleSubmit() on submission. A floater here will also display any validation errors. The date uses a date picker for ease of use as well. 
+  */
   return (
     <Fragment>
       <Container>
